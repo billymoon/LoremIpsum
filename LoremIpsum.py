7107adc9e7f143738a4269f2f893e1e6a6efa079
Billy Moon <billy@itaccess.org>
2013-03-26 01:00:09 +0300

    bumped version, added features

--- LoremIpsum.py
+++ LoremIpsum.py
@@ -1,23 +1,56 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import random
+import re
+
 
 class LoremIpsumCommand(sublime_plugin.TextCommand):
-    def run(self, edit, qty = 10):
+
+    def run(self, edit, qty=10):
+
         selections = self.view.sel()
         for selection in selections:
-            # always start with Lorem ipsum
-            para = "Lorem ipsum ";
+
+            # always start with Lorem ipsum for first outpur lorem
+            para = "Lorem ipsum "
+
             # words from the original Lorum ipsum text
             words = "dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".split()
 
-            from random import choice
-            for x in range(random.randint(qty - qty/2, qty + qty/2)):
-                para += choice(words) + " "
-            para += choice(words) + ". "
-            editor = self.view.begin_edit()
+            lastchars = self.view.substr(sublime.Region(selection.begin()-20, selection.end()))
+            last = re.search("(|(\d+)(\.\d+)*)$", lastchars).group(0)
+            if re.search("\d", last):
+                print "will repeat " + last + " times"
+                # + len(last)
+                selection = sublime.Region(selection.begin() - len(str(last)), selection.end())
+            else:
+                last = 1
+
+            m = str(last).split(".")
+            paras = int(m[0])
+
+            if len(m) > 1:
+                qty = int(m[1])
+
+            for i in range(0, paras):
+                from random import choice
+                para += choice(words).capitalize() + " "
+                for x in range(random.randint((qty - qty/3)-2, (qty + qty/3)-2)):
+                    para += choice(words) + " "
+                para += choice(words) + "."
+                if i != paras and paras > 1:
+                    para += "\n\n"
+                editor = self.view.begin_edit()
+
+            print len(str(last))
 
             # erase region
             self.view.erase(editor, selection)
+
+            last = self.view.substr(sublime.Region(selection.begin()-1, selection.end()))
+            if last == ".":
+                para = " " + para
+
             # insert para before current cursor position
             self.view.insert(editor, selection.begin(), para)
 
--- README.markdown
+++ README.markdown
@@ -4,18 +4,30 @@ An extension for Sublime Text 2.
 
 It allow you to Insert Lorem Ipsum in the editor via menu items or keyboard shortcut.
 
+Select how much text you want from the menu item in `Edit->Text->Lorem Ipsum` or on the right click menu in `Lorem Ipsum`.
+
 Just press the shortcut key (Alt+Command+l on OSX, Control+Alt+l on Windows/Linux) to add Lorem Ipsum text. Keep pressing to add more.
 
-Alternatively, select how much text you want from the menu item.
+Alternatively, you can also specify the number of paragraphs, by writing a number, and pressing the shortcut key. Lorem Ipsum will check if there is a number directly preceding the selection range, and replace it with that many paragraphs of lorem ipsum. Addind a decimal point, defines the approximate number of words per paragraph.
+
+## Examples
+
+Generate a dummy title for a blog, and a few paragraphs
+
+- Press the shortcut once, to generate a single line of lorem upsum, of about 10 characters
+- Move down to a newline, and type `5.60` and then press the keyboard shortcut for lorem ipsum
 
 ## Features
 
-1. Uses only words from the original Lorem ipsum text.
+1. Uses only words from [the original Lorem ipsum text](http://www.lipsum.com/)
 2. Randomly generates text from selection of words
-3. Always starts with `Lorem ipsum`
+3. Always starts with `Lorem ipsum` for first paragraph, and capitalizes following paragraphs
 4. Leaves the inserted text un-selected, so you can insert some more straight away (rapid tapping to get as much lorem as you want)
 5. Shortcut key, `Edit -> Text -> Lorem Ipsum...` sub-menu, and context menu activation
 6. Variable size of text from just `a bit` to more than you can shake a stick at!
+7. *NEW*: Specify how many paragraphs and words per paragraph you want (type a number before pressing the lorem ipsum keyboard shortcut)
+8. Works with multiple selection ranges
+9. *NEW*: Does not leave trailing spaces
 
 ## Install
 

6c5f48c404e483e53aaf14120f67db0e7a233393
Billy Moon <billy@itaccess.org>
2013-07-01 23:38:37 +0100

    make it work on ST3, and add sanity check for amount of lorem ipsum generated at one time

--- LoremIpsum.py
+++ LoremIpsum.py
@@ -18,13 +18,21 @@ class LoremIpsumCommand(sublime_plugin.TextCommand):
             words = "dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".split()
 
             lastchars = self.view.substr(sublime.Region(selection.begin()-20, selection.end()))
-            last = re.search("(|(\d+)(\.\d+)*)$", lastchars).group(0)
-            if re.search("\d", last):
-                print "will repeat " + last + " times"
+            last = re.search("(|(\d+)(\.\d+)?)$", lastchars).group(0)
+
+            m = str(last).split(".")
+
+            if re.search("\d", last) and (
+                (len(m) > 1 and (int(m[0]) * int(m[1])) < 1000)
+                or (len(m) == 1 and int(m[0]) < 1000)
+            ):
+                print("will repeat " + last + " times")
                 # + len(last)
                 selection = sublime.Region(selection.begin() - len(str(last)), selection.end())
             else:
                 last = 1
+            # else:
+            #     print("[ERROR] too much lorem ipsum - try a smaller number")
 
             m = str(last).split(".")
             paras = int(m[0])
@@ -32,29 +40,29 @@ class LoremIpsumCommand(sublime_plugin.TextCommand):
             if len(m) > 1:
                 qty = int(m[1])
 
-            for i in range(0, paras):
+            for i in list(range(0, paras)):
                 from random import choice
                 para += choice(words).capitalize() + " "
-                for x in range(random.randint((qty - qty/3)-2, (qty + qty/3)-2)):
+                print(">>> >>> " + str(qty))
+                for x in list(range(random.randint(int(qty - qty/3)-2, int(qty + qty/3)-2))):
                     para += choice(words) + " "
                 para += choice(words) + "."
                 if i != paras and paras > 1:
                     para += "\n\n"
-                editor = self.view.begin_edit()
 
-            print len(str(last))
+            print(len(str(last)))
 
             # erase region
-            self.view.erase(editor, selection)
+            self.view.erase(edit, selection)
 
             last = self.view.substr(sublime.Region(selection.begin()-1, selection.end()))
             if last == ".":
                 para = " " + para
 
             # insert para before current cursor position
-            self.view.insert(editor, selection.begin(), para)
+            self.view.insert(edit, selection.begin(), para)
 
             # insert para over the top of selection, but remaining selected (not behavior we want)
             # self.view.replace(editor, self.view.sel()[0], para)
 
-            self.view.end_edit(editor)
+            self.view.end_edit(edit)
